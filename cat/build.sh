61e0f2b70e6c99a5131301191604ebc8bd3fc78e
Carl Dunkelberger <carl.dunkelberger@docker.com>
2019-06-18 10:59:35 -0400

    cleaned up extraneous files

--- cat/build.sh
+++
@@ -1,2 +0,0 @@
-docker build --tag cdunkelb/https-nginx:cat-v5 .
-docker push cdunkelb/https-nginx:cat-v5
--- dog/build.sh
+++
@@ -1,2 +0,0 @@
-docker build --tag cdunkelb/https-nginx:dog-v5 .
-docker push cdunkelb/https-nginx:dog-v5
