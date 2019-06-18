db7a5582be2a9899b4d6aaf5bb24a11b9015056a
Carl Dunkelberger <carl.dunkelberger@docker.com>
2019-06-18 10:53:00 -0400

    cdunkelbs version

---
+++ cat/build.sh
@@ -0,0 +1,2 @@
+docker build --tag cdunkelb/https-nginx:cat-v5 .
+docker push cdunkelb/https-nginx:cat-v5
---
+++ dog/build.sh
@@ -0,0 +1,2 @@
+docker build --tag cdunkelb/https-nginx:dog-v5 .
+docker push cdunkelb/https-nginx:dog-v5
